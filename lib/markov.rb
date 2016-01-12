eefaee82a65b33d4f753fc67e4d9b93ce734af31
Michael Kuehl <michael@ratchet.cc>
2016-01-12 13:04:31 +0100

    Version bump to 0.0.0

---
+++ lib/markov.rb
@@ -0,0 +1,11 @@
+
+module Markov
+  
+  require 'markov/util'
+  
+  #def generator(depth=3)
+  #  return Markov::Generator.new(depth)
+  #end
+  
+  #module_function :generator
+end
---
+++ lib/markov/util.rb
@@ -0,0 +1,3 @@
+
+module Markov::Util
+end
